a443515d6436d01d6c15ddcc016c467597ec41c6
Andrew Vreeland <122303931+AndrewVreeland@users.noreply.github.com>
2023-07-26 18:17:41 -0400

    fixed Chat GPT issues, added amplify

---
+++ Quizzler2/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "quizzler2": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
--- Quizzler2/app/src/main/java/com/study/quizzler2/fragments/ChatFragment.java
+++ Quizzler2/app/src/main/java/com/study/quizzler2/fragments/ChatFragment.java
@@ -1,5 +1,6 @@
 package com.study.quizzler2.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,15 +16,33 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.study.quizzler2.MainActivity;
 import com.study.quizzler2.R;
 import com.study.quizzler2.adapters.MessageAdapter;
+import com.study.quizzler2.helpers.Config;
 import com.study.quizzler2.helpers.Message;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class ChatFragment extends Fragment {
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
 
     private RecyclerView recyclerView;
     private TextView welcomeTextView;
@@ -33,7 +52,7 @@ public class ChatFragment extends Fragment {
     private MessageAdapter messageAdapter;
 
 
-
+    OkHttpClient client = new OkHttpClient();
     public ChatFragment() {
         // Required empty public constructor
     }
@@ -57,7 +76,8 @@ public class ChatFragment extends Fragment {
             String question = messageEditText.getText().toString().trim();
             addToChat(question, Message.SENT_BY_ME);
             messageEditText.setText("");
-            // Call the API or any other logic to handle the user message
+            Context thisContext = requireActivity().getApplicationContext();
+           callAPI(question, thisContext);
         });
     }
 
@@ -67,11 +87,70 @@ public class ChatFragment extends Fragment {
         recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
     }
 
-
+    void addResponse(String response){
+        messageList.remove(messageList.size()-1);
+        addToChat(response, Message.SENT_BY_BOT);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+
+    void callAPI(String question, Context context) {
+        messageList.add(new Message("Typing...", Message.SENT_BY_BOT));
+
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("model", "text-davinci-003");
+            jsonBody.put("prompt", question);
+            jsonBody.put("max_tokens", 4000);
+            jsonBody.put("temperature", 0);
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        RequestBody body = RequestBody.create(jsonBody.toString(), JSON);
+
+        String apiKey = Config.getApiKey(context);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/completions")
+                .header("Authorization", "Bearer " + apiKey)
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                requireActivity().runOnUiThread(() -> addResponse("failed to load response due to " + e.getMessage()));
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(response.body().string());
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        String result = jsonArray.getJSONObject(0).getString("text");
+                        requireActivity().runOnUiThread(() -> addResponse(result.trim()));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                } else {
+                    requireActivity().runOnUiThread(() -> {
+                        try {
+                            addResponse("failed to load response due to " + response.body().string());
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                }
+            }
+        });
+
     }
 }
