d7596d8d255cf1348f044cbbb38dec3b089a8753
Andrew Vreeland <122303931+AndrewVreeland@users.noreply.github.com>
2023-08-06 14:10:18 -0400

    added a hamburger menu, this is where the user will be able to logout of their account and where the chat history will be for the user

--- Quizzler2/app/src/main/java/com/study/quizzler2/MainActivity.java
+++ Quizzler2/app/src/main/java/com/study/quizzler2/MainActivity.java
@@ -1,17 +1,35 @@
 package com.study.quizzler2;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 import com.study.quizzler2.fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        // Set up the drawer toggle
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Enable the hamburger icon
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -19,9 +37,25 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
         }
 
-
-
+        // Set up item click listener for the navigation view
+        navigationView.setNavigationItemSelectedListener(item -> {
+            // Handle navigation view item clicks here.
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    // Do something for home
+                    break;
+                // Handle other ids
+            }
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
     }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
