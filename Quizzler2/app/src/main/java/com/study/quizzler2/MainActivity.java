9ce548b39edda8db38c4b96f83f9c468a4652f01
Andrew Vreeland <122303931+AndrewVreeland@users.noreply.github.com>
2023-08-06 14:29:38 -0400

    added the back button functionality to return to the previous screen when openeing the hamburger menu

--- Quizzler2/app/src/main/java/com/study/quizzler2/MainActivity.java
+++ Quizzler2/app/src/main/java/com/study/quizzler2/MainActivity.java
@@ -58,4 +58,14 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
